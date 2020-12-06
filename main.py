207157146f54fee34e54c5bfb556be6b8354b180
Thilakshan Arulnesan <thilakshan@brokerbay.ca>
2020-12-05 19:11:37 -0500

    add solutions for d3

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from solutions import d1, d2
+from solutions import d1, d2, d3
 
-print("--- Day 1 ---")
+# print("--- Day 1 ---")
 # d1.part1()
 # d1.part2()
 
-print("--- Day 2 ---")
-d2.part1()
-d2.part2()
+# print("--- Day 2 ---")
+# d2.part1()
+# d2.part2()
+
+print("--- Day 3 ---")
+
+d3.part1()
+d3.part2()
---
+++ solutions/d3.py
@@ -0,0 +1,42 @@
+
+from utils.read_file import read_file
+
+
+def is_tree(char):
+    return char == "#"
+
+
+def find_num_trees(data, down=1, right=3):
+    width = len(data[0])
+    num_to_check = 0
+    total_trees = 0
+
+    for i in range(0, len(data), down):
+        line = data[i]
+        # if()
+        if(is_tree(line[num_to_check])):
+            total_trees += 1
+        num_to_check = (num_to_check + right) % width
+
+    return total_trees
+
+
+def part1():
+    data = read_file('data/d3.in')
+    num_trees = find_num_trees(data, 1, 3)
+    print(num_trees)
+
+
+def part2():
+    data = read_file('data/d3.in')
+    slopes = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]
+    ]
+    answer = 1
+    for slope in slopes:
+        answer *= find_num_trees(data, slope[1], slope[0])
+    print(answer)
